7619f7d648a9d6eeb714e4d7f5a214aa9114c44c
brian <brianmuturi2@gmail.com>
2024-08-15 13:50:14 +0300

    feat(ioc container): accessing ioc container and decorators

---
+++ src/scheduler/scheduler.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { DiscoveryModule } from '@nestjs/core';
+import { IntervalScheduler } from './interval.scheduler/interval.scheduler';
+
+@Module({
+    imports: [DiscoveryModule],
+    providers: [
+        IntervalScheduler
+    ]
+})
+export class SchedulerModule {}
