8c2ca37fc61cf9ae1e7f5d984e46eb9035e79c44
brian <brianmuturi2@gmail.com>
2024-08-15 12:23:03 +0300

    feat(advanced concepts): scaffold project, lazy load rewards service

---
+++ src/coffees/coffees.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { COFFEES_DATA_SOURCE, CoffeesService } from './coffees.service';
+import { CoffeesController } from './coffees.controller';
+
+
+@Module({
+  controllers: [CoffeesController],
+  providers: [CoffeesService, {provide: COFFEES_DATA_SOURCE, useValue: []}],
+})
+export class CoffeesModule {}
