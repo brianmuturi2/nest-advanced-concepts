ed1f10fae1b93b44e134a02120e4e7ba8e5e0824
brian <brianmuturi2@gmail.com>
2024-08-16 12:40:44 +0300

    feat(circuit breaker): implement circuit breaker pattern

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, RequestTimeoutException } from '@nestjs/common';
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+import { CircuitBreakerInterceptor } from 'src/common/interceptors/circuit-breaker/circuit-breaker.interceptor';
 
+@UseInterceptors(CircuitBreakerInterceptor)
 @Controller('coffees')
 export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) {}
@@ -14,7 +16,10 @@ export class CoffeesController {
 
   @Get()
   findAll() {
-    return this.coffeesService.findAll();
+    //return this.coffeesService.findAll();
+    // Simulator error to be handled by circuit breaker
+    console.log('"findAll" executed');
+    throw new RequestTimeoutException('Error!');
   }
 
   @Get(':id')
